549677fc4c3999a5edc4652c5a605f7e3ef51eb2
Md. Toukir Ahmed Mehedi <cooltoukir@gmail.com>
2024-05-29 15:49:23 +0600

    tutorial done

--- site/src/jsMain/kotlin/com/toukirahmed/websiteone/AppEntry.kt
+++ site/src/jsMain/kotlin/com/toukirahmed/websiteone/AppEntry.kt
@@ -1,13 +1,41 @@
 package com.toukirahmed.websiteone
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.palette.background
+import com.varabyte.kobweb.silk.theme.colors.palette.button
+import com.varabyte.kobweb.silk.theme.colors.palette.color
+import com.varabyte.kobweb.silk.theme.colors.palette.link
 import org.jetbrains.compose.web.css.*
+
+@InitSilk
+fun overrideSilkTheme(ctx: InitSilkContext) {
+    ctx.theme.palettes.light.background = Colors.White
+    ctx.theme.palettes.light.color = Colors.Black
+    ctx.theme.palettes.light.link.default = Colors.Black
+    ctx.theme.palettes.light.link.visited = Colors.Blue
+    ctx.theme.palettes.light.button.default = Colors.DarkGray
+    ctx.theme.palettes.light.button.hover = Colors.LightGray
+    ctx.theme.palettes.light.button.focus = Colors.LightGray
+    ctx.theme.palettes.light.button.pressed = Colors.Gray
+
+    ctx.theme.palettes.dark.background = Colors.Black
+    ctx.theme.palettes.dark.color = Colors.White
+    ctx.theme.palettes.dark.link.default = Colors.White
+    ctx.theme.palettes.dark.link.visited = Colors.Blue
+    ctx.theme.palettes.dark.button.default = Colors.Blue
+    ctx.theme.palettes.dark.button.hover = Colors.MediumBlue
+    ctx.theme.palettes.dark.button.focus = Colors.MediumBlue
+    ctx.theme.palettes.dark.button.pressed = Colors.DarkBlue
+}
 
 @App
 @Composable
--- site/src/jsMain/kotlin/com/toukirahmed/websiteone/pages/Index.kt
+++ site/src/jsMain/kotlin/com/toukirahmed/websiteone/pages/Index.kt
@@ -1,18 +1,98 @@
 package com.toukirahmed.websiteone.pages
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.css.CSSTransition
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.selectors.focus
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.placeholder
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    var name by remember { mutableStateOf("") }
+    var colorMode by ColorMode.currentState
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            FaFaceSmile(
+                modifier = Modifier
+                    .color(if (colorMode.isLight) Colors.Black else Theme.Blue.color)
+                    .margin(right = 10.px),
+                style = IconStyle.OUTLINE,
+                size = IconSize.XXL
+            )
+            P(
+                attrs = Modifier.fontSize(40.px).toAttrs()
+            ) {
+                Text(value = "Hello, $name")
+            }
+        }
+        Input(
+            type = InputType.Text,
+            attrs = InputStyle
+                .toModifier()
+                .padding(topBottom = 10.px, leftRight = 20.px)
+                .outline(width = 0.px, style = LineStyle.None, color = Colors.Transparent)
+                .backgroundColor(Theme.Gray.color)
+                .fontSize(20.px)
+                .transition(CSSTransition(property = "border", duration = 300.ms))
+                .toAttrs {
+                    placeholder(value = "Enter your name")
+                    onInput {
+                        name = it.value
+                    }
+                }
+        )
+        Button(
+            modifier = Modifier.margin(top = 24.px),
+            onClick = {colorMode = colorMode.opposite}
+        ) {
+            Text(value = "Change Theme")
+        }
     }
+}
+
+val InputStyle = CssStyle {
+    base {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Theme.Gray.color
+        )
+    }
+    focus {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Theme.Blue.color
+        )
+    }
+}
+
+enum class Theme(val color: CSSColorValue) {
+    Gray(color = rgb(r = 238, g = 238, b = 238)),
+    Blue(color = rgb(r = 28, g = 281, b = 224))
 }
